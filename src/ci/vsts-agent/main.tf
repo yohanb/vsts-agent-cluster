bda7c6cd1f9a7268cb4f5bca0ce5e0b3a64430bf
Yohan Belval <yohan.belval@gmail.com>
2018-08-21 12:24:02 -0400

    Added default vars to vsts ansible role.

--- src/ci/vsts-agent/main.tf
+++ src/ci/vsts-agent/main.tf
@@ -14,7 +14,7 @@ output "image_id" {
 
 # Create a Resource Group for the new Virtual Machine.
 resource "azurerm_resource_group" "main" {
-  name     = "${var.vm_resource_group}"
+  name     = "${var.resource_group}"
   location = "canadaeast"
 }
 
@@ -36,10 +36,11 @@ resource "azurerm_subnet" "internal" {
 
 # Create a Public IP for the Virtual Machine
 resource "azurerm_public_ip" "main" {
-  name                         = "${var.vm_name}-pip"
+  name                         = "${var.vm_name}-pip--${count.index}"
   location                     = "${azurerm_resource_group.main.location}"
   resource_group_name          = "${azurerm_resource_group.main.name}"
   public_ip_address_allocation = "dynamic"
+  count                        = "${local.instance_count}"
 }
 
 # Create a Network Security Group with some rules
@@ -64,35 +65,37 @@ resource "azurerm_network_security_group" "main" {
 
 # Create a network interface for VMs and attach the PIP and the NSG
 resource "azurerm_network_interface" "main" {
-  name                      = "${var.vm_name}-nic"
+  name                      = "${var.vm_name}-nic-${count.index}"
   location                  = "${azurerm_resource_group.main.location}"
   resource_group_name       = "${azurerm_resource_group.main.name}"
   network_security_group_id = "${azurerm_network_security_group.main.id}"
+  count                     = "${local.instance_count}"
 
   ip_configuration {
     name                          = "primary"
     subnet_id                     = "${azurerm_subnet.internal.id}"
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = "${azurerm_public_ip.main.id}"
+    public_ip_address_id          = "${element(azurerm_public_ip.main.*.id, count.index)}"
   }
 }
 
 # Create a new Virtual Machine based on the Golden Image
 resource "azurerm_virtual_machine" "vm" {
-  name                             = "${var.vm_name}"
+  name                             = "${var.vm_name}-${count.index}"
   location                         = "${azurerm_resource_group.main.location}"
   resource_group_name              = "${azurerm_resource_group.main.name}"
-  network_interface_ids            = ["${azurerm_network_interface.main.id}"]
-  vm_size                          = "Standard_F2s_v2"
+  network_interface_ids            = ["${element(azurerm_network_interface.main.*.id, count.index)}"]
+  vm_size                          = "Standard_B2s"
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
+  count                            = "${local.instance_count}"
 
   storage_image_reference {
     id = "${data.azurerm_image.search.id}"
   }
 
   storage_os_disk {
-    name              = "${var.vm_name}-osdisk"
+    name              = "${var.vm_name}-${count.index}-osdisk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
@@ -100,7 +103,7 @@ resource "azurerm_virtual_machine" "vm" {
   }
 
   storage_data_disk {
-    name              = "${var.vm_name}-data1"
+    name              = "${var.vm_name}-${count.index}-data"
     managed_disk_type = "Standard_LRS"
     create_option     = "Empty"
     lun               = 0
@@ -108,7 +111,7 @@ resource "azurerm_virtual_machine" "vm" {
   }
 
   os_profile {
-    computer_name  = "${var.vm_name}"
+    computer_name  = "${var.vm_name}-${count.index}"
     admin_username = "${var.admin_username}"
   }
 
@@ -122,12 +125,11 @@ resource "azurerm_virtual_machine" "vm" {
 }
 
 data "template_file" "agent_command" {
-  template = "$${cd} && $${config} && $${install_svc} && $${start_svc}"
+  template = "$${config} && $${install_svc} && $${start_svc}"
   vars {
-    cd = "cd /a1"
-    config = "./bin/Agent.Listener configure --unattended --url https://${var.vsts_account_name}.visualstudio.com --auth pat --token ${var.vsts_pat} --pool default --agent ${var.vm_name} --acceptTeeEula"
-    install_svc = "./svc.sh install"
-    start_svc = "./svc.sh start"
+    config = "/a1/bin/Agent.Listener configure --unattended --url https://${var.vsts_account_name}.visualstudio.com --auth pat --token ${var.vsts_pat} --pool default --agent $(hostname) --acceptTeeEula"
+    install_svc = "/a1/svc.sh install"
+    start_svc = "/a1/svc.sh start"
   }
 }
 
@@ -135,10 +137,11 @@ resource "azurerm_virtual_machine_extension" "agent_script" {
   name                 = "agent_script"
   location             = "${azurerm_resource_group.main.location}"
   resource_group_name  = "${azurerm_resource_group.main.name}"
-  virtual_machine_name = "${azurerm_virtual_machine.vm.name}"
+  virtual_machine_name = "${element(azurerm_virtual_machine.vm.*.name, count.index)}"
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
+  count                = "${local.instance_count}"
 
   settings = <<SETTINGS
     {
